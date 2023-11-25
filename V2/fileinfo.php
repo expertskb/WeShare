a1c53c4bd625ee05c54ed1d70052ba0c0b45ca2d
Shakib Ahmed <112560247+expertskb@users.noreply.github.com>
2023-11-25 22:50:16 +0600

    Update fileinfo.php

--- V2/fileinfo.php
+++ V2/fileinfo.php
@@ -5,4 +5,4 @@ include('class/weshare.class.php');
 use Shakib\WeShare;
 
 $class = new WeShare(""); // Enter Your Api Token
-var_dump($class->FileInfo("iHApQZyMN2"));
+var_dump($class->FileInfo("")); // Enter Your File ID
